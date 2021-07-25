b9ab019f2675d6e5f0f4dd2129eee699be2aebb9
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2021-07-25 16:47:17 -0400

    added factories for invoices

--- multiviral/sales/tests/test_invoices.py
+++ multiviral/sales/tests/test_invoices.py
@@ -4,9 +4,10 @@ from django.test import TestCase
 
 # Django REST Framework
 from rest_framework.test import APIClient
+from rest_framework import status
 
-# Factories 
-# from multiviral.inventory.factories import ProductFactory
+# factories 
+from multiviral.sales.factories import InvoiceDetailFactory, InvoiceFactory
 
 class InvoicesTest(TestCase):
   
@@ -17,4 +18,14 @@ class InvoicesTest(TestCase):
     
   def test_users_can_generate_new_invoices_with_pending_status(self):
     
+    
+    invoice = InvoiceFactory.create()
+    
+    for _ in range(0, 5):
+      InvoiceDetailFactory.create(invoice=invoice)
+    
     response = self.client.post('/api/invoice/')
+    
+    self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
+    self.assertEqual(len(response.data), 1)
+    
