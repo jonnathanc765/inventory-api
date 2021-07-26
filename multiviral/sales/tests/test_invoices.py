ae5fc9f50d904511780e9f7fa629f7c18b31d4e6
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2021-07-26 17:38:36 -0400

    testing with products

--- multiviral/sales/tests/test_invoices.py
+++ multiviral/sales/tests/test_invoices.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 # factories 
 from multiviral.sales.factories import InvoiceDetailFactory, InvoiceFactory
+from multiviral.inventory.factories import ProductFactory
 
 class InvoicesTest(TestCase):
   
@@ -29,4 +30,35 @@ class InvoicesTest(TestCase):
     self.assertTrue(len(response.data[0]['invoice_details']), 5)
     self.assertTrue('name' in response.data[0]['invoice_details'][0]['product'])
     self.assertEqual(len(response.data[0]['invoice_details']), 5)
+    
+  def test_users_receive_404_code_if_invoices_does_not_exists(self):
+    
+    response = self.client.get('/api/sales/invoices/232/')
+    self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+  def test_invoice_details_are_saving_details_of_products(self):
+    
+    invoice = InvoiceFactory.create(
+      status='F',
+    )
+    
+    product = ProductFactory.create()
+    
+    InvoiceDetailFactory.create(
+      invoice=invoice, 
+      product=product, 
+      product_name=product.name,
+      product_price=product.sell_price,
+    )
+    
+    product.delete()
+    
+    response = self.client.get(f"/api/sales/invoices/{invoice.pk}/")
+    
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(response.data['invoice_details'][0]['product'], None)
+    self.assertEqual(response.data['invoice_details'][0]['product_name'], product.name)
+    self.assertEqual(response.data['invoice_details'][0]['product_price'], str(product.sell_price) + '.00')
+    
+    
     
